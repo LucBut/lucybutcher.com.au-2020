d3bd8dccabaebccc145c38f5fd095795ea0abf1a
Johnny Noble <github-a3bd@johnny.net.au>
2021-04-03 11:59:23 +1100

    Un-Lato this thing
    https://react.semantic-ui.com/theming/
    
    Needed to downgrade the craco version because of react-scripts 3.4.0. But it seems to work just fine.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
+/* eslint-disable max-len */
 import React from 'react';
 import { Container, Divider, Header, Image } from 'semantic-ui-react';
 
-import 'semantic-ui-css/semantic.min.css';
+// import 'semantic-ui-css/semantic.min.css';
+import 'semantic-ui-less/semantic.less';
 
 const App = () => {
   return (
     <Container text style={{ marginTop: '50px', marginBottom: '150px' }}>
       {/* <Image src="/images/Circle_FINAL-1200px.png" centered size="huge" /> */}
 
-      <Image src="/images/Lucy Butcher_hi-res square-min.png" centered size="huge" />
+      <Image
+        src="/images/Lucy Butcher_hi-res square-min.png"
+        centered
+        size="huge"
+      />
 
       {/* <Header as="h2">Lucy Butcher</Header> */}
 
       <Divider />
 
       <Header as="h2">What am I?</Header>
+      <p>Freelance copy editor / proofreader</p>
       <p>
-      Freelance copy editor / proofreader
+        Specialising in science, health and medicine (but all subjects welcome)
       </p>
-      <p> Specialising in science, health and medicine (but all subjects welcome)</p>
       <p>Science graduate / AWARD School graduate</p>
 
       <Divider />
@@ -38,7 +44,9 @@ const App = () => {
       <Header as="h2">A bit more about me</Header>
 
       <p>
-        <b>I’m an experienced writer.</b> But I’ve realised that I prefer editing. Improving a document’s accuracy and readability makes me strangely happy. So for now, this is my focus. 
+        <b>I’m an experienced writer.</b> But I’ve realised that I prefer
+        editing. Improving a document’s accuracy and readability makes me
+        strangely happy. So for now, this is my focus.
       </p>
       <p>
         <b>I like what I do.</b> I’m always learning and this keeps me curious.
@@ -48,7 +56,9 @@ const App = () => {
         means I can do my best for each client.
       </p>
       <p>
-        <b>When I'm working on your project it has my full attention.</b> I sit in a quiet home office, not an open-plan workplace with Spotify blaring. I can really concentrate.
+        <b>When I'm working on your project it has my full attention.</b> I sit
+        in a quiet home office, not an open-plan workplace with Spotify blaring.
+        I can really concentrate.
       </p>
       <p>
         <b>Work-life balance is important.</b> This is why I freelance. I don't
@@ -58,16 +68,25 @@ const App = () => {
       </p>
 
       <p>
-        <b>I don’t email my portfolio.</b> I respect client confidentiality and Rx communications regulations. But I’m always happy to chat about my experience.
+        <b>I don’t email my portfolio.</b> I respect client confidentiality and
+        Rx communications regulations. But I’m always happy to chat about my
+        experience.
       </p>
       <p>
-        <b>I believe in science-based medicine and critical thinking.</b> I loathe Instagram influencers and their unreferenced health claims. I don’t work on anything dodgy or with flimsy evidence.</p>
-        <p>You know who you
-        are. You probably use words like ‘natural’, ‘organic’ or ‘chemical free’
-        in your marketing (for more information, see{' '}
-        <a href="https://isitchemicalfree.com">isitchemicalfree.com</a>). </p>
-      <p>You may even subscribe to the religious health teachings of people like Pete Evans
-        or Andrew Wakefield. Either way, I’m not the freelancer for you.</p>
+        <b>I believe in science-based medicine and critical thinking.</b> I
+        loathe Instagram influencers and their unreferenced health claims. I
+        don’t work on anything dodgy or with flimsy evidence.
+      </p>
+      <p>
+        You know who you are. You probably use words like ‘natural’, ‘organic’
+        or ‘chemical free’ in your marketing (for more information, see{' '}
+        <a href="https://isitchemicalfree.com">isitchemicalfree.com</a>).
+      </p>
+      <p>
+        You may even subscribe to the religious health teachings of people like
+        Pete Evans or Andrew Wakefield. Either way, I’m not the freelancer for
+        you.
+      </p>
 
       <Divider />
       <Header as="h2">Testimonials</Header>
